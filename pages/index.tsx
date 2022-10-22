0c4d4480abd8cc52c0ffb036b904a355ce63ca7a
joshua constine <joshua.constine97@gmail.com>
2022-10-22 13:25:54 -0700

    added motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,30 @@ import { useState } from "react";
 import Button from "../components/button";
 import Card from "../components/card";
 import Navbar from "../components/navbar";
+import Avatar from "@mui/material/Avatar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import Typography from "@mui/material/Typography";
 import styles from "../styles/Home.module.css";
 
+const filters = ["mexican", "american", "sushi", "subs", "coffee"];
 const Home: NextPage = () => {
   const [showDialog, setShowDialog] = useState(false);
   const toggleDialog = () => {
     setShowDialog(!showDialog);
   };
+  const handleClose = () => {
+    console.log("handleClose");
+    setShowDialog(!showDialog);
+  };
+  const handleListItemClick = (value: string) => {
+    console.log("handleClose", value);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -24,21 +41,9 @@ const Home: NextPage = () => {
         <Navbar />
         <h1>Great spots in San Diego</h1>
         <Button onClick={toggleDialog}>Filters</Button>
-        <Card
-          id="34"
-          size="small"
-          imgUrl="https://sparksgallery.com/wp-content/uploads/2021/11/best-restaurants-downtown-gaslamp-san-diego.jpg"
-        />
-        <Card
-          id="34"
-          size="small"
-          imgUrl="https://i0.wp.com/sparksgallery.com/wp-content/uploads/2021/11/lionfish.jpg?resize=1024%2C576&ssl=1"
-        />
-        <Card
-          id="34"
-          size="large"
-          imgUrl="https://i0.wp.com/sparksgallery.com/wp-content/uploads/2021/11/grant-grill.jpg?resize=1024%2C576&ssl=1"
-        />
+        <Card id="34" size="small" imgUrl="/static/restaurant1.jpg" />
+        <Card id="34" size="small" imgUrl="/static/restaurant2.jpg" />
+        <Card id="34" size="large" imgUrl="/static/restaurant3.jpg" />
       </main>
 
       <footer className={styles.footer}>
@@ -50,6 +55,19 @@ const Home: NextPage = () => {
           Created with passion
         </a>
       </footer>
+      <Dialog onClose={handleClose} open={showDialog}>
+        <List sx={{ pt: 0 }}>
+          {filters.map((filter) => (
+            <ListItem
+              button
+              onClick={() => handleListItemClick(filter)}
+              key={filter}
+            >
+              <ListItemText primary={filter} />
+            </ListItem>
+          ))}
+        </List>
+      </Dialog>
     </div>
   );
 };
